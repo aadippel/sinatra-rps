e5b50220f503c8edd5a67935e0d99d1624b13dc9
aadippel <aadippel@gmail.com>
2025-01-30 01:11:21 +0000

    started intro code

--- app.rb
+++ app.rb
@@ -3,7 +3,24 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Howdy!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/rock") do
+# write some code to make it match the target
+moves = ["rock", "paper", "scissors"]
+
+@comp_move = moves.sample
+
+if @comp_move == "rock"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @outcome == "lost"
+else 
+  @outcome == "won"
+end
+
+erb (:zebra)
 end
