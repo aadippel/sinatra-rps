1fe5015d99512691f6c975c924152814c562ad13
aadippel <aadippel@gmail.com>
2025-01-30 01:58:18 +0000

    added paths to layout.erb

--- app.rb
+++ app.rb
@@ -9,18 +9,16 @@ get("/") do
 end
 
 get("/rock") do
-# write some code to make it match the target
-moves = ["rock", "paper", "scissors"]
-
-@comp_move = moves.sample
-
-if @comp_move == "rock"
-  @outcome = "tied"
-elsif @comp_move == "paper"
-  @outcome = "lost"
-else 
-  @outcome = "won"
-end
-
-erb (:zebra)
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+  
+  erb (:zebra)
 end
